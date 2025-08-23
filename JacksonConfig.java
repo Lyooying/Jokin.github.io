8dc4e7c852ec04d3684517bcbb2e2eccbb5fc7de
野道书法家 <1412634973@qq.com>
2025-08-23 20:48:32 +0800

    Update JacksonConfig.java
    添加支持Date类型的日期格式转换

--- JacksonConfig.java
+++ JacksonConfig.java
@@ -1,13 +1,18 @@
 package cn.kgc.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.TimeZone;
 
 @Configuration
@@ -19,7 +24,7 @@ public class JacksonConfig {
     // 创建 LocalDateTime 序列化器
     private static final LocalDateTimeSerializer LOCAL_DATETIME_SERIALIZER =
             new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT));
-    
+
     // 创建 LocalDateTime 反序列化器
     private static final LocalDateTimeDeserializer LOCAL_DATETIME_DESERIALIZER =
             new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT));
@@ -31,7 +36,8 @@ public class JacksonConfig {
                 .serializers(LOCAL_DATETIME_SERIALIZER) // 注册自定义序列化器
                 .deserializers(LOCAL_DATETIME_DESERIALIZER) // 注册自定义反序列化器
                 .serializationInclusion(JsonInclude.Include.NON_NULL) // 忽略 null 值
-//                .timeZone(TimeZone.getDefault()); // 添加默认时区设置, 默认为操作系统的时区
+//                .dateFormat(new SimpleDateFormat(DATE_TIME_FORMAT)) // 设置日期格式, 同时支持Date类型
+                .simpleDateFormat(DATE_TIME_FORMAT) // 设置日期格式, 同时支持Date类型
                 .timeZone(TimeZone.getTimeZone("Asia/Shanghai")); // 明确指定东八区
     }
 }
